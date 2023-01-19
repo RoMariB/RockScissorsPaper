c19b09e08a73c14517790f6783c5bbb51f61003f
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-01-19 21:44:09 +0700

    Initial Commit

---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by Мария Романчук on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
